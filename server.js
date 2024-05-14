96b025a2c3c435525abfd22a8ce802f1110c2877
Bwlake <152343852+Bwlake@users.noreply.github.com>
2024-05-14 18:55:27 +0000

    Fixing error with server.js

--- server.js
+++ server.js
@@ -8,56 +8,56 @@ const __dirname = join(fileURLToPath(import.meta.url), "..");
 const bare = createBareServer("/bare/");
 const app = express();
 
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 
 app.use((req, res) => {
-    res.status(404);
-    res.sendFile(join(__dirname, "public", "404.html")); 
+  res.status(404);
+  res.sendFile(join(__dirname, "public", "404.html"));
 });
 
 const server = createServer();
 
 server.on("request", (req, res) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeRequest(req, res);
-    } else {
-        app(req, res);
-    }
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
 });
 
 server.on("upgrade", (req, socket, head) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeUpgrade(req, socket, head);
-    } else {
-        socket.end();
-    }
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 8080; 
+if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
-    const address = server.address();
-    console.log("Listening on:");
-    console.log(`\thttp://localhost:${address.port}`);
-    console.log(
-        `\thttp://${
-            address.family === "IPv6" ? `[${address.address}]` : address.address
-        }:${address.port}`
-    );
+  const address = server.address();
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`,
+  );
 });
 
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
-    console.log("SIGTERM signal received: closing HTTP server");
-    server.close();
-    bare.close();
-    process.exit(0);
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
 }
 
 server.listen({
-    port,
+  port,
 });
