fcfb1c1776de3493cdc2e807820a4db104311898
Bwlake <bwigal2@nastudents.org>
2024-06-09 18:13:10 -0400

    Updating Bare Server

--- public/u/config.js
+++ public/u/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/p/",
-  bare: "https://lunarlearningcenter.space/bare/",
+  bare: "/b/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/u/handler.js",
